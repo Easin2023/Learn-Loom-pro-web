62ed2cd4020d0f47c75aff1d481b47c15cc1d04e
Easin2023 <mdeasinarafat016456@gmail.com>
2023-11-30 23:26:14 +0600

    vercel api added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const config = axios.create({
-     baseURL: 'http://localhost:5000'
-     // baseURL: 'https://learn-room-pro-server.vercel.app'
+     // baseURL: 'http://localhost:5000'
+     baseURL: 'https://learn-room-pro-server.vercel.app'
 })
 
 const useAxiosSecure = () => {
